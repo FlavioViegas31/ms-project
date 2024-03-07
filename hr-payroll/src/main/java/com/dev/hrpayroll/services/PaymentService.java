c9be094dad90bf7315f15317d216f79f114683fe
FlavioViegas31 <flavio.viegas31@gmail.com>
2024-03-07 16:04:17 -0300

    Feign Client

--- hr-payroll/src/main/java/com/dev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/dev/hrpayroll/services/PaymentService.java
@@ -1,14 +1,28 @@
 package com.dev.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.dev.hrpayroll.entities.Payment;
+import com.dev.hrpayroll.entities.Worker;
+import com.dev.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+				
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 				
 	}
 }
