ca3afc2a76998f76c911bcb4a888c4a4d287f9a0
FlavioViegas31 <flavio.viegas31@gmail.com>
2024-03-13 21:59:01 -0300

    Security Configuris to config server

--- hr-api-gateway-zuul/src/main/java/com/dev/hrpayroll/apigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/dev/hrpayroll/apigatewayzuul/config/ResourceServerConfig.java
@@ -16,11 +16,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] PUBLIC = {"hr-oauth/oauth/token" };
+	private static final String[] PUBLIC = {"/hr-oauth/oauth/token" };
 	
 	private static final String[] OPERATOR = {"/hr-worker/**" };
 	
-	private static final String[] ADMIN = {"/hr-payroll/**", "/hr-user/**" };
+	private static final String[] ADMIN = {"/hr-payroll/**", "/hr-user/**", "/actuator/**", "/hr_worker/actuator/**", "/hr_oauth/actuator/**" };
 	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
